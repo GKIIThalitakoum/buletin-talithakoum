6ed3de2220bf0bc868edc9336d72ed4cdae64d6b
GKIIThalitakoum <gkiithalitakoum@gmail.com>
2026-08-08 15:16:56 +0800

    Tambah config.js

---
+++ src/styles/src/lib/config.js
@@ -0,0 +1,16 @@
+// GANTI baris di bawah ini dengan Spreadsheet ID Google Sheets Anda.
+// ID diambil dari URL sheet, bagian antara "/d/" dan "/edit".
+export const SPREADSHEET_ID = "GANTI_DENGAN_ID_SHEET_ANDA";
+
+export const SHEET_NAMES = {
+  EDISI: "Edisi",
+  JADWAL: "Jadwal_Kegiatan",
+  PETUGAS: "Petugas",
+  SUSUNAN: "Susunan_Ibadah",
+  POKOK_DOA: "Pokok_Doa",
+  INFORMASI: "Informasi",
+  KEUANGAN: "Keuangan",
+  KONTAK: "Kontak_Pengurus",
+};
+
+export const NAMA_GEREJA = "GKII Jemaat Talitha Koum Sendawar";
