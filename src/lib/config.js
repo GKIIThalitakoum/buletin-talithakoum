44bcb503e05e30ac321d10e2b3d5c4e4dee4e40e
GKIIThalitakoum <gkiithalitakoum@gmail.com>
2026-08-10 13:49:58 +0800

    Sambungkan ke Google Sheets asli

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,16 +1,19 @@
-// GANTI baris di bawah ini dengan Spreadsheet ID Google Sheets Anda.
-// ID diambil dari URL sheet, bagian antara "/d/" dan "/edit".
-export const SPREADSHEET_ID = "GANTI_DENGAN_ID_SHEET_ANDA";
+// URL dasar hasil "Publish to web" (File > Share > Publish to web).
+// Diambil dari kotak link yang muncul, bagian sebelum "?output=...".
+export const PUBLISHED_BASE_URL =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vRGAEeE3Krym8T5OSZU28lC8Wzs3kK70mKUHwq3RGnMDaCiaoo_gGy0EwY1CZDGPa7gVmb7igOj9nc0/pub";
 
-export const SHEET_NAMES = {
-  EDISI: "Edisi",
-  JADWAL: "Jadwal_Kegiatan",
-  PETUGAS: "Petugas",
-  SUSUNAN: "Susunan_Ibadah",
-  POKOK_DOA: "Pokok_Doa",
-  INFORMASI: "Informasi",
-  KEUANGAN: "Keuangan",
-  KONTAK: "Kontak_Pengurus",
+// Kode "gid" tiap tab. Diambil dari URL saat tab tersebut aktif dibuka,
+// bagian setelah "#gid=".
+export const SHEET_GIDS = {
+  EDISI: "0",
+  JADWAL: "1375390797",
+  PETUGAS: "384021789",
+  SUSUNAN: "1606517273",
+  POKOK_DOA: "708674348",
+  INFORMASI: "538121873",
+  KEUANGAN: "1460671467",
+  KONTAK: "1083480543",
 };
 
 export const NAMA_GEREJA = "GKII Jemaat Talitha Koum Sendawar";
